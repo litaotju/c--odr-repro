5ec7b7caa4feef4a67142eefed78f858c527b14d
Tao Li <litaotju@live.cn>
2021-11-12 00:35:50 +0800

    Use fpic

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
-g++ use_v1.cpp -c -g
-g++ use_v2.cpp -c -g
+g++ use_v1.cpp -c -g -fPIC
+g++ use_v2.cpp -c -g -fPIC
 
-g++ use_v1.o use_v2.o  -shared -g -o libuse_v1_first.so 
-g++ use_v2.o use_v1.o -shared -g -o libuse_v2_first.so 
+g++ use_v1.o use_v2.o -fPIC -shared -g -o libuse_v1_first.so 
+g++ use_v2.o use_v1.o -fPIC -shared -g -o libuse_v2_first.so 
 
 g++ -g main.cpp -luse_v1_first -L./ -o main_v1_first
 g++ -g main.cpp -luse_v2_first -L./ -o main_v2_first
